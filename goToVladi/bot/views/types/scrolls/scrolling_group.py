4eb1956a0bf307d949eafcd03e6c99fd8a0535a2
rSlow <redmaxx4@gmail.com>
2025-02-22 14:49:06 +1000

    fix scrolling group pager

--- goToVladi/bot/views/types/scrolls/scrolling_group.py
+++ goToVladi/bot/views/types/scrolls/scrolling_group.py
@@ -6,4 +6,5 @@ from aiogram_dialog.widgets.kbd import ScrollingGroup as BaseScrollingGroup
 class ScrollingGroup(BaseScrollingGroup):
     async def _render_pager(self, pages: int, manager: DialogManager) -> RawKeyboard:
         pager = await super()._render_pager(pages, manager)
-        return pager[1:-1]
+        pager[0] = pager[0][1:-1]
+        return pager
